4f7bc2229000da4f88ffe9b19be0f7dc85bf244a
Blayne Marjama <blayne.marjama@gmail.com>
2024-11-08 15:07:41 -0800

    moved deliver scene

--- scripts/Enemy/Goals/EnemyGoal_DeliverItem.gd
+++ scripts/Enemy/Goals/EnemyGoal_DeliverItem.gd
@@ -1,23 +1,37 @@
 class_name EnemyGoalDeliverItem
 extends EnemyGoal
 
-@export_category("SubGoals")
-@export var item: GoalItem;
-@export var dropoff: EnemyGoalDropOffItem
-@export var pickup: EnemyGoalPickupItem
+var item: CarriableEnemyGoalItem;
+var dropoff: EnemyGoalDropOffItem
+var pickup: EnemyGoalPickupItem
+@export var dropoff_marker: Marker3D
+@export var item_marker: Marker3D
+@export var item_color: Color = Color.BLUE
 
 func _ready() -> void:
 	for child in get_children():
-		if child is GoalItem:
+		if child is CarriableEnemyGoalItem:
 			item = child
 		if child is EnemyGoalDropOffItem:
 			dropoff = child
 		if child is EnemyGoalPickupItem:
 			pickup = child
 	
+	# Set the item for the pickup and dropoff goals.
 	pickup.goal_item = item;
 	dropoff.goal_item = item;
+	# Set the locations of the item and dropoff
+	set_item_spawn(item_marker.global_position)
+	set_dropoff_location(dropoff_marker.global_position)
+	# Set item color
+	item.change_mesh_color(item_color)
 
 # TODO: Find a better way than this??
 func get_sub_goal_order() -> Array[EnemyGoal]:
 	return [pickup, dropoff]
+
+func set_item_spawn(location: Vector3) -> void:
+	item.global_position = location
+
+func set_dropoff_location(location: Vector3) -> void:
+	dropoff.global_position = location
--- scripts/Items/CarriableEnemyGoalItem.gd
+++ scripts/Items/CarriableEnemyGoalItem.gd
@@ -2,6 +2,7 @@ class_name CarriableEnemyGoalItem
 extends GoalItem
 
 @onready var collision_shape := %CollisionShape3D
+@onready var mesh := %MeshInstance3D
 # This can be either the Player marker, Enemy marker, or null.
 var carry_object_marker: Marker3D = null;
 var carrier: CharacterBody3D = null;
@@ -19,9 +20,15 @@ func drop_item() -> void:
 	carry_object_marker = null;
 	carrier = null;
 
+func change_mesh_color(new_color: Color) -> void:
+	var new_material := StandardMaterial3D.new()
+	new_material.albedo_color = new_color
+	# If we use the same material, it will be reused for all isntances of the box.
+	mesh.set_surface_override_material(0, new_material)
+
 func _physics_process(delta: float) -> void:
 	if carrier == null:
 		collision_shape.disabled = false
 	else:
-		self.position = carry_object_marker.global_position
+		self.global_position = carry_object_marker.global_position
 		collision_shape.disabled = true
